ce19e88ef895252fd23dafda7dc3694b707ee04e
Chris Burgess <chrisburgess@catalyst.net.nz>
2018-07-27 15:24:47 +1200

    ✨ WRMS search

--- src/wrms.js
+++ src/wrms.js
@@ -12,15 +12,34 @@
 // Commands:
 //   hubot wrms me <issue-id> - Show the issue status
 
-const creds = {
-  endpoint: process.env.HUBOT_WRMS_URL,
-  username: process.env.HUBOT_WRMS_USERNAME,
-  password: process.env.HUBOT_WRMS_PASSWORD
-}
-
 module.exports = function(robot) {
-    robot.respond(/wrms me (?:issue )?(?:#)?(\d+)/i, function(msg){
-        const id = msg.match[1]
-        msg.reply(`${process.env.HUBOT_WRMS_URL}/${id}`)
-    });
+  const creds = {
+    endpoint: process.env.HUBOT_WRMS_URL,
+    username: process.env.HUBOT_WRMS_USERNAME,
+    password: process.env.HUBOT_WRMS_PASSWORD
+  }
+  const WRMS = require('wrms')
+  const wrms = new WRMS(creds)
+
+  robot.respond(/wrms me (?:issue )?(?:#)?(\d+)/i, (msg) => {
+    const id = msg.match[1]
+    msg.reply(`${process.env.HUBOT_WRMS_URL}/${id}`)
+  })
+
+  robot.respond(/wrms search (.*)$/i, (msg) => {
+    const params = { q: msg.match[1] }
+    wrms.work_request.search(params)
+      .then((res) => {
+        if (!res.response.body.length) {
+          msg.reply('No results')
+        }
+        else {
+          let reply = `${res.response.body.length} results:\n`
+          res.response.body.forEach(result => {
+            reply += `* [${result.description}](${creds.endpoint}/${result.request_id})\n`
+          })
+          msg.reply(reply)
+        }
+      })
+  })
 }
