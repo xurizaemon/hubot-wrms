80ed9408782d7900be8626ffa5de60c636b5eb7b
Chris Burgess <chrisburgess@catalyst.net.nz>
2018-07-28 14:26:14 +1200

    ✨ Chime in on mentions of work requests by ID
    By default responds to `#123` as per @wormy, but you can configure
    HUBOT_WRMS_MENTION_REGEX to suit eg `WR#123`.
    
    Default is `/#(\d+)/`, use `HUBOT_WRMS_MENTION_REGEX='WR#(\d+)'` to
    do the latter.

--- src/wrms.js
+++ src/wrms.js
@@ -9,6 +9,8 @@
 //   HUBOT_WRMS_USERNAME=yourusername
 //   HUBOT_WRMS_PASSWORD=yourpassword
 //   HUBOT_WRMS_SEARCH_MAX=25
+//   HUBOT_WRMS_MENTION_REGEX='#(\d+)'
+//   HUBOT_WRMS_IGNORED_USERS=craig
 //
 // Commands:
 //   hubot wrms me <issue-id> - Show the issue status
@@ -27,6 +29,26 @@ module.exports = function(robot) {
   const WRMS = require('wrms')
   const wrms = new WRMS(creds)
 
+  const ignoredUsers = process.env.HUBOT_WRMS_IGNORED_USERS || ''
+
+  // Chime in when WR are mentioned.
+  const mentionsRegexp = process.env.HUBOT_WRMS_MENTION_REGEX || '#(\\d+)'
+  robot.hear(mentionsRegexp, (msg) => {
+    const id = msg.match[1].replace('#', '')
+    if (isNaN(id) || ignoredUsers.split(',').includes(msg.message.user.name)) {
+      return
+    }
+    wrms.work_request.get(id)
+      .then((res) => {
+        if (typeof res.response.brief !== 'undefined') {
+          const wr = res.response
+          const system = typeof wr.system.name !== 'undefined' ? wr.system.name : '?'
+          msg.send(`${wr.request_type_label} ${wr.request_id} (${system}): ${wr.brief} (${wr.last_status_label}) [${wr.urgency_label}]`)
+          msg.send(`${config.url}/${wr.request_id}`)
+        }
+      })
+  })
+
   robot.respond(/wrms me (?:issue )?(?:#)?(\d+)/i, (msg) => {
     const id = msg.match[1]
     msg.reply(`${process.env.HUBOT_WRMS_URL}/${id}`)
@@ -41,7 +63,6 @@ module.exports = function(robot) {
     msg.reply(`Searching WRMS for "${msg.match[3]}"`)
     wrms.work_request.search(params)
       .then((res) => {
-        robot.logger.debug(res, 'res')
         if (!res.response.body.length) {
           msg.reply('No results')
         }
